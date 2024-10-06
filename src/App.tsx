d79a49fd384efe403ca680e470a7ab01a918475f
Allester Corton <91792059+allestercorton@users.noreply.github.com>
2024-10-06 10:04:02 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
-import { Schedule } from './pages/Schedule';
-import { Standings } from './pages/Standings';
-import Home from './pages/Home';
 
 export default function App() {
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/schedule' element={<Schedule />} />
-          <Route path='/standings' element={<Standings />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import App from './App';
+import Home from './pages/Home';
+import { Schedule } from './pages/Schedule';
+import { Standings } from './pages/Standings';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'schedule',
+        element: <Schedule />,
+      },
+      {
+        path: 'standings',
+        element: <Standings />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
